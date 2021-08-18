724cfcd512820c9eb54104158a5b984b44069b0f
sina shariati <sina.shariati@yahoo.com>
2021-08-18 21:13:40 +0430

    Remove welcome.blade.php and send 404 when hitting with web request

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    throw new NotFoundHttpException();
 });
