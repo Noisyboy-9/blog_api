a6849d4818b5187f985c2c917cfccb39731560ee
sina shariati <sina.shariati@yahoo.com>
2021-08-19 18:38:37 +0430

    Refactor api routes to use Route::resource

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,4 @@
 
 use App\Http\Controllers\PostController;
 
-Route::post('/posts', [PostController::class, 'store']);
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{post}', [PostController::class, 'show']);
-Route::patch('/posts/{post}', [PostController::class, 'update']);
-Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+Route::resource('posts', PostController::class)->except('create', 'edit');
