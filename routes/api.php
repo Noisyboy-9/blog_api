ea5b6d2bf4734c32ad6a837c4136f88375ff22ac
sina shariati <sina.shariati@yahoo.com>
2021-08-27 16:22:41 +0430

    Cleanup api.php file

--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,11 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\posts\PostsCommentController;
 
-//comments
 Route::middleware('auth:sanctum')->group(function () {
-//    posts
     Route::apiResource('posts', PostController::class);
-
-//    comments
     Route::prefix('/posts/{post}')->group(function () {
         Route::apiResource("comments", PostsCommentController::class);
     });
-
-//    categories
     Route::apiResource('categories', CategoryController::class);
 });
 
